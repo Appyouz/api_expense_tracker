959f5e7190825413974c18d75281d1fec5d30535
appyouz <dikpalkhatri7@gmail.com>
2025-04-26 10:55:41 +0545

    Task:Populate expense category with predefined categories completed

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
+from typing import override
+
 from django.db import models
 
-# Create your models here.
+
+class ExpenseCategory(models.Model):
+    """
+    Represents a category for expenses (e.g., Groceries, Leisure).
+    """
+    name = models.CharField(max_length=100, unique=True)
+     
+    class Meta:
+        verbose_name_plural = 'Expense Categories'
+
+    def __str__(self) -> str:
+        """
+        Represents a category for expenses (e.g., Groceries, Leisure).
+        """
+        return self.name
