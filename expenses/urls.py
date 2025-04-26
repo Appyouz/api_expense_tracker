ef0840a53c66633fa45ca46aa1c11e49c2385067
appyouz <dikpalkhatri7@gmail.com>
2025-04-26 11:45:45 +0545

    task: created rotuer for api view url

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import UserRegistrationView
+from .views import ExpenseViewSet, UserRegistrationView
+
+router = DefaultRouter()
+router.register(r'expenses', ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user_register'),
+    path('', include(router.urls)),
 ]
